1d5e27baa2d2dd4442dfb9ad66700b43fd0996b1
taiki-fw <taiki_52@outlook.jp>
2021-04-25 23:53:55 +0900

    :recycle:Slack APIを叩くロジックをSDKからシンプルなrequestsに変更

--- app/Slack/util/lib.py
+++ app/Slack/util/lib.py
@@ -1,5 +1,15 @@
+import os
+import json
+import requests
+
 from ...lib.logger import logger
 from .index import client
+
+SLACK_BOT_USER_OAUTH_TOKEN = os.environ.get("BOT_USER_OAUTH_TOKEN")
+SLACK_HEADERS = {
+    "Content-Type": "application/json; charset=UTF-8",
+    "Authorization": "Bearer {0}".format(SLACK_BOT_USER_OAUTH_TOKEN),
+}
 
 
 def post_dm(user_slack_id: str, text="", blocks={}):
@@ -8,9 +18,29 @@ def post_dm(user_slack_id: str, text="", blocks={}):
     - chat:write
     - im:write
     """
-    dm_open_res = client.conversations_open(users=user_slack_id)
+    # dm_open_res = client.conversations_open(users=user_slack_id)
+    dm_open_res = requests.post(
+        "https://slack.com/api/conversations.open",
+        data=json.dumps({"token": SLACK_BOT_USER_OAUTH_TOKEN, "users": user_slack_id}),
+        headers=SLACK_HEADERS,
+    )
+    dm_open_res = dm_open_res.json()
+
     dm_channel_ids = dm_open_res["channel"]["id"]
 
-    res = client.chat_postMessage(channel=dm_channel_ids, text=text, blocks=blocks)
+    # res = client.chat_postMessage(channel=dm_channel_ids, text=text, blocks=blocks)
+    res = requests.post(
+        "https://slack.com/api/chat.postMessage",
+        data=json.dumps(
+            {
+                "token": SLACK_BOT_USER_OAUTH_TOKEN,
+                "channel": dm_channel_ids,
+                "text": text,
+                "blocks": blocks,
+            }
+        ),
+        headers=SLACK_HEADERS,
+    )
+    logger.warn(res.json())
 
-    return res
+    return res.json()
